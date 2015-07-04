aa11296241cc324265db37622c34239acd3d7f15
Christophe Willemsen <willemsen.christophe@gmail.com>
2015-07-05 00:03:36 +0200

    fix event

--- src/Converter/BaseConverter.php
+++ src/Converter/BaseConverter.php
@@ -21,7 +21,7 @@ abstract class BaseConverter implements EventConverterInterface
         $q = 'MERGE (user:User {id: {user_id}})
         ON CREATE SET user.login = {user_login}
         MERGE (event:GithubEvent {id: {event_id}})
-        SET event.type = {event_type}, event.time = {event_time}, user_id: {user_id}
+        SET event.type = {event_type}, event.time = {event_time}, event.user_id = {user_id}
         MERGE (et:EventType {type:{event_type}})
         MERGE (event)-[:EVENT_TYPE]->(et)
         RETURN event';
