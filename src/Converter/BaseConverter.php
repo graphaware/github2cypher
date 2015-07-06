710cb8a6dba495bd816c26bddb25b22ac1011fcd
Christophe Willemsen <willemsen.christophe@gmail.com>
2015-07-06 18:52:44 +0200

    removed event type and ll

--- src/Converter/BaseConverter.php
+++ src/Converter/BaseConverter.php
@@ -20,24 +20,14 @@ abstract class BaseConverter implements EventConverterInterface
     {
         $q = 'MERGE (user:User {id: {user_id}})
         ON CREATE SET user.login = {user_login}
-        CREATE (event:GithubEvent {id: {event_id}})
-        SET event.type = {event_type}, event.time = {event_time}, event.user_id = {user_id}
-        MERGE (et:EventType {type:{event_type}})
-        MERGE (event)-[:EVENT_TYPE]->(et)
-        WITH user, event
-        OPTIONAL MATCH (user)-[r:LAST_EVENT]->(lastEvent)
-        DELETE r
-        MERGE (user)-[:LAST_EVENT]->(event)
-        WITH user, event, collect(lastEvent) as lastEvents
-        FOREACH (x in lastEvents | CREATE (event)-[:PREVIOUS_EVENT]->(x))
-        RETURN event';
+        MERGE (event:GithubEvent {id: {event_id}})
+        ON CREATE SET event.time = {event_time}, event.user_id = {user_id}, event:' . $event->getEventType();
 
         $p = [
             'user_id' => $event->getActor()->getId(),
             'user_login' => $event->getActor()->getLogin(),
             'event_id' => $event->getEventId(),
             'event_time' => ($event->getCreatedTime()->getTimestamp() * 1000),
-            'event_type' => $event->getEventType()
         ];
 
         return [
